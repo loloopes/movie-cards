4429b314aff0194e589d9915cc6dcacc5659a737
Lopes <gus.lcampos38@gmail.com>
2021-09-19 21:48:40 -0300

    Updates

---
+++ precache-manifest.5684793d3458017bd54875dd719f265e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b18c10dc0723ca214d8fe4f57aa0476a",
+    "url": "/movie-cards/index.html"
+  },
+  {
+    "revision": "ce8dc50f9aea0df1f7c4",
+    "url": "/movie-cards/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2259dd06164684a032f6",
+    "url": "/movie-cards/static/js/2.ab6a279b.chunk.js"
+  },
+  {
+    "revision": "ce8dc50f9aea0df1f7c4",
+    "url": "/movie-cards/static/js/main.698a6037.chunk.js"
+  },
+  {
+    "revision": "e0ba7f353d36a6504002",
+    "url": "/movie-cards/static/js/runtime-main.f38ae7b4.js"
+  }
+]);
